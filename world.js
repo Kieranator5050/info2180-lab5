d5775bcbb5ee2c6af593bed01b444be9f9b36f56
Kieranator5050 <thejugg63@gmail.com>
2021-11-03 15:49:51 -0500

    Exercise 3 Updated

--- world.js
+++ world.js
@@ -2,4 +2,14 @@
 "use strict"
 $(document).ready(function(){
   //console.log("DOM Loaded");
+  $('#lookup').click(function(){
+  let inputTxt = $('#country').val().trim().replace(/[^a-z0-9áéíóúñü \.,_-]/gim, "");
+  $('#result').html("");
+  fetch(`./world.php?country=${inputTxt}`)
+    .then(response => response.text())
+    .then(data => {
+      $('#result').append(data);
+    })
+    .catch(err => console.error(err))
+  });
 });
