f1cddb0256b76877ef632344c9dca150f6803e9f
Kieranator5050 <thejugg63@gmail.com>
2021-11-03 19:14:28 -0500

    Exercise 5 Completed

--- world.js
+++ world.js
@@ -2,14 +2,33 @@
 "use strict"
 $(document).ready(function(){
   //console.log("DOM Loaded");
+
+  /*
+  AjaxCall Function
+  Params : requestType
+  Returns : none
+  Description
+  - Runs an ajex request using fetch to request the php file
+  - If true is passed in then the default lookup is used
+  - If false is passed in them the lookupCities request is used
+  */
+  function ajaxCall(requestType){
+
+    let inputTxt = $('#country').val().trim().replace(/[^a-z0-9áéíóúñü \.,_-]/gim, "");
+    $('#result').html("");
+    let url = requestType ? `./world.php?country=${inputTxt}&context=countries` : `./world.php?country=${inputTxt}&context=cities`;
+    fetch(url)
+      .then(response => response.text())
+      .then(data => {
+        $('#result').append(data);
+      })
+      .catch(err => console.error(err))
+  }
+
   $('#lookup').click(function(){
-  let inputTxt = $('#country').val().trim().replace(/[^a-z0-9áéíóúñü \.,_-]/gim, "");
-  $('#result').html("");
-  fetch(`./world.php?country=${inputTxt}`)
-    .then(response => response.text())
-    .then(data => {
-      $('#result').append(data);
-    })
-    .catch(err => console.error(err))
+    ajaxCall(true);
+  });
+  $('#lookupCity').click(function(){
+    ajaxCall(false);
   });
 });
--- world.php
+++ world.php
@@ -4,39 +4,68 @@ $username = 'lab5_user';
 $password = 'password123';
 $dbname = 'world';
 
-//Sanitize Querry String
+//Sanitize Querry Strings
 $country = filter_input(INPUT_GET, 'country', FILTER_SANITIZE_STRING);
+$context = filter_input(INPUT_GET, 'context', FILTER_SANITIZE_STRING);
 
 //Connect to db and make querry
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
-$stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
+$isCities = $context == "cities";
+if($isCities){
+  $stmt = $conn->query("SELECT cities.name, cities.district, cities.population FROM cities INNER JOIN countries ON cities.country_code=countries.code WHERE countries.name LIKE '%$country%'");
+} else {
+  $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
+}
 
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-//Generate Table
+//Generate Table Header
 echo "<table>";
-echo "<thead>
-  <tr>
-    <th>Country Name</th>
-    <th>Continent</th>
-    <th>Independence Year</th>
-    <th>Head of State</th>
-  </tr>
-</thead>";
-foreach ($results as $row){
-  //var_dump($row);
-  $name = $row['name'];
-  $continent = $row['continent'];
-  $independence_year = $row['independence_year'];
-  $head_of_state = $row['head_of_state'];
-  echo "<tbody>
-    <tr>
-      <td>$name</td>
-      <td>$continent</td>
-      <td>$independence_year</td>
-      <td>$head_of_state</td>
-    </tr>
-  </tbody>";
+echo "<thead>";
+echo    "<tr>";
+if($isCities){
+  echo      "<th>City Name</th>";
+  echo      "<th>District</th>";
+  echo      "<th>Population</th>";
+} else {
+  echo      "<th>Country Name</th>";
+  echo      "<th>Continent</th>";
+  echo      "<th>Independence Year</th>";
+  echo      "<th>Head of State</th>";
 }
+echo    "</tr>";
+echo "</thead>";
+
+//Generate Table Entries
+if($isCities){
+  foreach ($results as $row){
+    //var_dump($row);
+    $name = $row['name']; $district = $row['district'];
+    $population = $row['population'];
+    echo "<tbody>
+      <tr>
+        <td>$name</td>
+        <td>$district</td>
+        <td>$population</td>
+      </tr>
+    </tbody>";
+  }
+} else {
+  foreach ($results as $row){
+    //var_dump($row);
+    $name = $row['name']; $continent = $row['continent'];
+    $independence_year = $row['independence_year']; $head_of_state = $row['head_of_state'];
+    echo "<tbody>
+      <tr>
+        <td>$name</td>
+        <td>$continent</td>
+        <td>$independence_year</td>
+        <td>$head_of_state</td>
+      </tr>
+    </tbody>";
+  }
+}
+
+
 echo "</table>";
 ?>
